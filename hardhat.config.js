186683bae965df8310c3ecfc3729072194476a13
Sunny <swarnendughosh05@gmail.com>
2024-07-01 20:58:37 +0530

    script added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.9",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/YKSdR4zinthYBBGUxnLWWpH-sE6gRyO2",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
